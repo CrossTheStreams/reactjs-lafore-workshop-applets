7e75ecd48953153df66d6b98eebf922f77af672c
Andrew Hautau <arhautau@gmail.com>
2017-10-31 00:21:29 -0700

    some basic layout UI with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,118 @@
 import React, { Component } from 'react';
-import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { PageHeader, Grid, Col, 
+  Navbar, Jumbotron, Button, 
+  ListGroup, Nav, NavDropdown, 
+  MenuItem, Panel } from 'react-bootstrap';
+import { BrowserRouter } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">Lafore Workshop Applets</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron>
-      </div>
+      <BrowserRouter>
+        <div> 
+          <Navbar inverse fixedTop>
+            <Grid>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <a href="/">Lafore Workshop Applets</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Nav>
+                  <NavDropdown title="Workshop Applets" id="basic-nav-dropdown">
+                    <MenuItem href="/array">Array</MenuItem>
+                    <MenuItem divider />
+                    <MenuItem href="/ordered-array">OrderedArray</MenuItem>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Header>
+            </Grid>
+          </Navbar>
+          <Route path="/array" component={ArrayWS}/>
+          <Route path="/ordered-array" component={OrderedArrayWS}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
+
+class WorkshopLayout extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div class="WorkshopLayout container">
+        <Grid>
+          <PageHeader>
+            {this.props.title}
+          </PageHeader>
+          <Col xs={12} md={7}>
+            <Panel>
+              {this.props.leftPanelContent}
+            </Panel>
+          </Col>
+          <Col xs={6} md={5}>
+            <Panel>
+              {this.props.rightPanelContent}
+            </Panel>
+          </Col>
+        </Grid>
+      </div>
+    )
+  }
+
+}
+
+class ArrayWS extends Component {
+  render() {
+    return (
+      <WorkshopLayout
+        title={
+          <div>
+            <h1>Array</h1>
+          </div>
+        }
+        leftPanelContent={
+          <div>
+            <p>Some content for the left panel</p>
+          </div>
+        }
+        rightPanelContent={
+          <div>
+            <p>Some content for the right panel</p>
+          </div>
+        }
+      />
+    )
+  }
+}
+
+class OrderedArrayWS extends Component {
+  render() {
+    return(
+      <WorkshopLayout
+        title={
+          <div>
+            <h1>Ordered Array</h1>
+          </div>
+        }
+        leftPanelContent={
+          <div>
+            <p>Some content for the left panel</p>
+          </div>
+        }
+        rightPanelContent={
+          <div>
+            <p>Some content for the right panel</p>
+          </div>
+        }
+      />
+    )
+  }
+}
+
 
 export default App;
