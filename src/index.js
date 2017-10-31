fa8a168f56512d86b02ac463f5bd187f8cfef217
Andrew Hautau <arhautau@gmail.com>
2017-10-30 21:09:16 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
